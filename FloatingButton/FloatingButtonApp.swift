ee56e70231b0fa41529b944be6ae55abdb88428f
SabbirHossain07 <sabbirsopnil07@gmail.com>
2021-10-10 17:58:36 +0600

    Initial Commit

---
+++ FloatingButton/FloatingButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  FloatingButtonApp.swift
+//  FloatingButton
+//
+//  Created by Sopnil Sohan on 10/10/21.
+//
+
+import SwiftUI
+
+@main
+struct FloatingButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
